e55cb5110c6a6b6db24550ec924de1fbf6e8e090
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-12-05 10:27:02 +0000

    Change to attr reader

--- lib/rspec/flakey_spec.rb
+++ lib/rspec/flakey_spec.rb
@@ -7,21 +7,18 @@ module RSpec
   # RSpec::FlakeySpec - An instance of an indeterminate spec.
   #
   class FlakeySpec
-    attr_reader :messages, :attempts, :retry_count
+    attr_reader :messages, :attempts, :retry_count, :location
 
     def initialize(example, attempts, retry_count)
       @example = example
       @attempts = attempts
       @retry_count = retry_count
       @messages = RetryMessage.new(@example).exception_strings
+      @location = @example.location
     end
 
     def as_json
       { attempts:, retry_count:, location:, messages: }
-    end
-
-    def location
-      @example.location
     end
   end
 end
